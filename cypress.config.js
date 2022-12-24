ba1133e8bafe29a4e7a1ef350c775640f55b74c2
Leslie Low <leslielowgy@gmail.com>
2022-12-25 01:47:41 +0800

    Setup basic mock http request tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ntk7rc',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    
+    specPattern: 'cypress/integration/examples/*.js'
   },
 });
